d0308f66c3fbdcdbdcf59170a7ed7bb211a2c912
Martin Coetzee <martin@martincoetzee.com>
2025-02-28 22:44:53 +1000

    added terraform

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "plugins" },
